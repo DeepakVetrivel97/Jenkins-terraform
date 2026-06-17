752c565c30aed793bd9209ad40069c4cbb1e9227
DeepakVetrivel97 <deepak.v6@cognizant.com>
2026-06-17 17:18:52 +0530

    Update c2-variables.tf

--- 01_VPC_terraform-manifests/c2-variables.tf
+++ 01_VPC_terraform-manifests/c2-variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region to deploy resources  region"
   type        = string
   default     = "us-east-1"
 }
